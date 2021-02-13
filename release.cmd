c38adadd2bf0fd6884bb68418fae8703b9b1fa03
JustOff <Off.Just.Off@gmail.com>
2021-02-13 17:39:11 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.5.11
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/active-stop-button-.+?\.xpi/download\/%VER%\/active-stop-button-%VER%\.xpi/" update.xml
+
+set XPI=active-stop-button-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
